15e900a4631d9834e747c021b0094ecfcaf36cac
Carles San Agustin <carles.sam@gmail.com>
2017-11-24 09:54:44 +0100

    fix path

--- create_start.sh
+++ create_start.sh
@@ -3,4 +3,4 @@
 wget https://gist.githubusercontent.com/carlessanagustin/dc8e6a47be27faaa3dbf99caa2a89458/raw/5d6a80af5f6c802ad772f64a89ba853cdb080f8e/create_keys.sh
 /bin/bash create_keys.sh
 
-/bin/bash ./run.sh
+/bin/bash $(pwd)/run.sh
